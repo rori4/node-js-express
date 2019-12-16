9f2ff060bc8a1d66ec403826b2871df359a923d6
rori4 <rangelstoilov@gmail.com>
2019-12-16 21:22:53 +0200

    html as res

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
+const http = require('http')
+const url = require('url')
 const fs = require('fs')
+const port = 1337
 
+http.createServer((req, res) => {
+	const path = url.parse(req.url).pathname
 
-// fs.writeFile('test.txt','Hello, there nodejs file system!', function(err){
-// 	if(err){
-// 		return console.log(err)
-// 	}
-// 	console.log('File successfully created!')
-// })
+	res.writeHead(200,{
+		'content-type': 'text/html'
+	})
 
+	if(path === '/'){
+		getHTMLResponse('./home.html', res)
+	} else if(path === '/about'){
+		getHTMLResponse('./about.html', res)
+	}	
+}).listen(port);
 
-fs.readFile('./index.html', (err, data) => {
-	if(err) {
-		return console.log(err)
-	}
-	console.log(data.toString())
-})
+function getHTMLResponse(path, res) { 
+	fs.readFile(path,(err, data) => {
+		if(err){
+			console.log(err)
+			return
+		}
+		res.write(data)
+		res.end()
+	})
+}
+
+console.log(`Server running at http://localhost:${port}`)
